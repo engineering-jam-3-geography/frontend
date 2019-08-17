d6050dca07c4ffbf2969a906214eed2358d0f62f
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-08-17 14:40:39 +0300

    Add "answersReducer"

---
+++ src/redux/reducers/answers/index.js
@@ -0,0 +1,28 @@
+// import actionTypes from '../../actions/types';
+
+const initialState = {
+    list: []
+};
+
+/**
+ * Answers reducer
+ * @param {Object} state
+ * @param {Object} action
+ * @returns {Object}
+ */
+const answersReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+    // case actionTypes.SHOW_PASSWORD_MODAL:
+    //     return {
+    //         ...state,
+    //         passwordModal: {
+    //             isHidden: false
+    //         }
+    //     };
+
+    default:
+        return state;
+    }
+};
+
+export default answersReducer;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import modalsReducer from './modals';
 import tasksReducer from './tasks';
+import answersReducer from './answers';
 
 const rootReducer = combineReducers({
     modals: modalsReducer,
-    tasks: tasksReducer
+    tasks: tasksReducer,
+    answers: answersReducer
 });
 
 export default rootReducer;
