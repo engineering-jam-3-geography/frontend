15665bd84e11e406f4d5487c3675b48ad7c55dc9
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-08-17 14:39:05 +0300

    Add "tasksReducer"

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import modalsReducer from './modals';
+import tasksReducer from './tasks';
 
 const rootReducer = combineReducers({
     modals: modalsReducer,
+    tasks: tasksReducer
 });
 
 export default rootReducer;
---
+++ src/redux/reducers/tasks/index.js
@@ -0,0 +1,29 @@
+// import actionTypes from '../../actions/types';
+
+const initialState = {
+    list: [],
+    activeTaskId: ''
+};
+
+/**
+ * Tasks reducer
+ * @param {Object} state
+ * @param {Object} action
+ * @returns {Object}
+ */
+const tasksReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+    // case actionTypes.SHOW_PASSWORD_MODAL:
+    //     return {
+    //         ...state,
+    //         passwordModal: {
+    //             isHidden: false
+    //         }
+    //     };
+
+    default:
+        return state;
+    }
+};
+
+export default tasksReducer;
